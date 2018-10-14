abdb1a552a24ef1c01a72ab8d930aeedcdce4767
Aleksandar Krsteski <acikabubo@gmail.com>
2018-10-14 21:53:04 +0200

    WIP: set nginx config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pratki-heroku
+
+Set 127.0.0.1 pratki in /etc/hosts and you can access from browser only with 'pratki' url
--- scripts/console.sh
+++ scripts/console.sh
@@ -3,18 +3,28 @@
 IMIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC="$(dirname "$IMIN")"
 
+docker network create pratki-net
+
 docker build --force-rm -t pratki-heroku \
     --build-arg USER=$USER \
     --build-arg UID=`id -u $USER` \
     --build-arg GID=`id -g $GROUP` \
     $SRC
-
+    
 docker create --rm -it \
     --name pratki-heroku \
     -v $SRC:/pratki-heroku \
     --hostname server \
+    --network pratki-net \
     -p 5000:5000 \
     pratki-heroku tmux new -s server bash
+
+docker run \
+    --name pratki-nginx \
+    -v $SRC/nginx/nginx.conf:/etc/nginx/conf.d/default.conf \
+    --network pratki-net \
+    -p 80:80 \
+    nginx:stable-alpine
 
 docker start -a -i pratki-heroku
 
@@ -23,6 +33,8 @@ if [ "x""$DANGLING" != "x" ]; then
     docker rmi $DANGLING
 fi
 docker volume ls -qf dangling=true | xargs -r docker volume rm
+
+docker rm -f pratki-nginx
 
 echo "Successfuly destroyed all containers"
 exit 0
