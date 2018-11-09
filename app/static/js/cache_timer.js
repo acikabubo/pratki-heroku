d13dc13eea2c6444696766d7119ad6d2d97dc9af
Aleksandar Krsteski <acikabubo@gmail.com>
2018-11-09 23:30:32 +0100

    Stop cache time and clean templates

--- app/static/js/cache_timer.js
+++ app/static/js/cache_timer.js
@@ -1,17 +1,13 @@
 $('#info-body').ready(function() {
-    // Function to update counters on all elements with class counter
-    var frame = function() {
+    var frame = setInterval(function(){
         $('.cache-timer').each(function() {
             var count = parseInt($(this).html());
             if (count !== 1) {
                 $(this).html(--count);
             } else {
+                clearInterval(frame);
                 $(this).html('');
-                return false;
             }
         });
-    };
-
-    // Schedule the update to happen once every second
-    setInterval(frame, 1000);
+    }, 1000);
 });
