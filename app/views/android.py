2ddaa6e40b9d69e911f561750d352fe68024a7b9
Aleksandar Krsteski <acikabubo@gmail.com>
2018-11-10 15:24:19 +0100

    Make small adjustment

--- app/views/android.py
+++ app/views/android.py
@@ -23,12 +23,12 @@ def android():
     # Decode bytes to string
     data = res.content.decode("utf-8")
 
+    packages = []
     if not data:
-        return '<h1 align="center">There is no packages</h1>'
+        return jsonify(packages)
 
     pkgs = [item.split(' - ') for item in data.split('\n')]
 
-    packages = []
     waiting, arrived = 0, 0
     for track_no, pkg_date, pkg_name in pkgs:
         send_date = parse(pkg_date, dayfirst=True)
@@ -103,7 +103,8 @@ def android():
 
     # # Return raw json data (for android app)
     # if request.user_agent.platform == 'android' and request.is_json:
-    return jsonify(packages)
+    if request.is_json:
+        return jsonify(packages)
 
 
 @app.route('/android/<track_no>/')
@@ -117,7 +118,7 @@ def android_detail(track_no):
     }
 
     if len(track_no) != 13:
-        return render_template('android_detail.html', data=data)
+        return render_template('package.html', data=data)
 
     r = requests.get(
         'http://www.posta.com.mk/tnt/api/query?id=%s' % track_no)
@@ -126,7 +127,7 @@ def android_detail(track_no):
     req_data = xmltodict.parse(r.text)
 
     if not req_data['ArrayOfTrackingData']:
-        return render_template('android_detail.html', data=data)
+        return render_template('package.html', data=data)
 
     # Get required data
     tracking_data = req_data['ArrayOfTrackingData']['TrackingData']
@@ -149,4 +150,8 @@ def android_detail(track_no):
             'notice': row[4][1]
         })
 
-    return render_template('android_detail.html', data=data)
+    # Return raw json data
+    if request.is_json:
+        return jsonify(data)
+
+    return render_template('package.html', data=data)
